12003f9e93d489e7772d5ad8df6e6b3cc91d90be
Patrick Swink <36057011+pjs6747@users.noreply.github.com>
2019-10-04 09:02:51 -0400

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,14 @@
+
+
+# L =
+#
+def fibPow(n):
+    # return pow(L(0,1), n)
+
+def L(a,b):
+    if b == 1:
+        return [0,1]
+    elif a == 1 && b == 1:
+        return [1,1]
+    else:
+        return [[1,1],[1,0]]
