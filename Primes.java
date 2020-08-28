30231c38ee5a99da04e80749991249209681e55b
Patrick Swink <36057011+pjs6747@users.noreply.github.com>
2020-08-28 11:21:54 -0400

    Add files via upload

---
+++ Primes.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Primes {
+    private static boolean isPrime(int test) {
+        if ( test <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < test; i++) {
+            if (test % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void printPrimes(int num) {
+        for (int test = 2; test <= num; test++) {
+            if (isPrime(test)) {
+                System.out.println(test);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number less than 10000");
+        int num = Integer.parseInt(scanner.nextLine());
+        if (num < 10000) {
+            printPrimes(num);
+        }
+    }
+}
---
+++ SmallestTwo.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SmallestTwo {
+    public static void main(String[] args) {
+        try {
+            File file = new File("test.txt");
+            Scanner sc = new Scanner(file);
+
+            int n = Integer.parseInt(sc.nextLine());
+            int lowest = Integer.MAX_VALUE;
+            int secondLowest = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                int next =  Integer.parseInt(sc.nextLine());
+                if (next != lowest && next != secondLowest) {
+                    if (next < secondLowest) {
+                        if (next < lowest) {
+                            secondLowest = lowest;
+                            lowest = next;
+                        } else {
+                            secondLowest = next;
+                        }
+                    }
+                }
+            }
+
+            System.out.println(lowest);
+            System.out.println(secondLowest);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
